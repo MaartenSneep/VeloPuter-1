4eb254a1f400f6af6ee23a5e690c4bd33a7f3279
Maarten Sneep <maarten.sneep@xs4all.nl>
2017-10-21 22:31:27 +0200

    Code formatting for readability Use byte values for intensity defaults

--- Arduino/VeloPuter/Constants.h
+++ Arduino/VeloPuter/Constants.h
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <U8glib.h>
 
-#define ICB_DF   // Configuration management: STRADA QUILTJE QUATRO and ICB_DF
+#define ICB_DF // Configuration management: STRADA QUILTJE QUATRO and ICB_DF
 
 #define Ug82
 
@@ -39,168 +39,168 @@
 
 */
 
-const byte switchCadPin =       0;      // NOTE: 0 and 1 are the RX and TX pins.
-const byte switchSpdPin =       1;      // NOTE: 0 and 1 are the RX and TX pins.
-const byte OLED_SDA =           2;      // used for the oled display through u8glib
-const byte OLED_SCL =           3;      // used for the oled display through u8glib
-const byte switchConfigPin =    4;
-const byte ledHeadPin =         5;
-const byte ledAuxPin =          6;
-const byte switchBrakePin =     7;
-const byte switchAlarmPin =     8;
-const byte ledRightPin =        9;
-const byte ledLeftPin =         10;
-const byte ledRearPin =         11;
-const byte UNUSED3 =            12;
-const byte speakerPin =         13;
-const byte voltagePin =         A0;
+const byte switchCadPin =    0; // NOTE: 0 and 1 are the RX and TX pins.
+const byte switchSpdPin =    1; // NOTE: 0 and 1 are the RX and TX pins.
+const byte OLED_SDA =        2; // used for the oled display through u8glib
+const byte OLED_SCL =        3; // used for the oled display through u8glib
+const byte switchConfigPin = 4;
+const byte ledHeadPin =      5;
+const byte ledAuxPin =       6;
+const byte switchBrakePin =  7;
+const byte switchAlarmPin =  8;
+const byte ledRightPin =     9;
+const byte ledLeftPin =     10;
+const byte ledRearPin =     11;
+const byte UNUSED3 =        12;
+const byte speakerPin =     13;
+const byte voltagePin =     A0;
 
 #if defined(STRADA)
-const byte switchHeadUpPin =    A2;
-const byte switchHeadDownPin =  A1;
-const byte switchRightPin =     A3;
-const byte switchLeftPin =      A4;
+const byte switchHeadUpPin   = A2;
+const byte switchHeadDownPin = A1;
+const byte switchRightPin    = A3;
+const byte switchLeftPin     = A4;
 #elif defined(QUILTJE)
-const byte switchRightPin =     A3; //A1;
-const byte switchLeftPin =      A4; //A2;
-const byte switchHeadDownPin =  A1; //A3;
-const byte switchHeadUpPin =    A2; //A4;
-#elif defined(QUATRO) ||defined(ICB_DF)
-const byte switchRightPin =     A1;
-const byte switchLeftPin =      A2;
-const byte switchHeadDownPin =  A3;
-const byte switchHeadUpPin =    A4;
+const byte switchRightPin    = A3; //A1;
+const byte switchLeftPin     = A4; //A2;
+const byte switchHeadDownPin = A1; //A3;
+const byte switchHeadUpPin   = A2; //A4;
+#elif defined(QUATRO) || defined(ICB_DF)
+const byte switchRightPin    = A1;
+const byte switchLeftPin     = A2;
+const byte switchHeadDownPin = A3;
+const byte switchHeadUpPin   = A4;
 #endif
-const byte UNUSED4 =            A5;
-
+const byte UNUSED4           = A5;
 
 /*
    Other constants which are used throughout the programm:
 */
-const long tSleep_ms = 300000;              // Timeout until sleep when cadence and speed sensor are active
-const long tSleepNoCadSpd_ms = 1800000;     // Timeout until sleep when cadence and speed sensor are NOT active
-const int  tPeriodBlink_ms = 333;           // 1.5 Hz Note that it actually is have a period.
-const byte numTimesToBlink = 5;             // 7 times high, 6 times low, = 13 = 4.3 s
+const long tSleep_ms = 300000; // Timeout until sleep when cadence and speed sensor are active
+const long tSleepNoCadSpd_ms = 1800000; // Timeout until sleep when cadence and speed sensor are NOT active
+const int tPeriodBlink_ms = 333; // 1.5 Hz Note that it actually is half a period.
+const byte numTimesToBlink = 6; // 7 times high, 6 times low, = 13 = 4.3 s
 //
 #if defined(STRADA)
-const int  wheelCircumference_mm = 1590;    // 406-50 wheel
-const int  rearWheelCircumference_mm = 2075; // 559-50 wheel
+const word wheelCircumference_mm     = 1590; // 406-50 wheel
+const word rearWheelCircumference_mm = 2075; // 559-50 wheel
 #elif defined(QUILTJE)
-const int  wheelCircumference_mm = 1450;    // 406-28 wheel
-const int  rearWheelCircumference_mm = 1990; // 559-35 wheel
+const word wheelCircumference_mm     = 1450; // 406-28 wheel
+const word rearWheelCircumference_mm = 1990; // 559-35 wheel
 #elif defined(QUATRO)
-const int  wheelCircumference_mm = 1540;   // 406-40 wheel
-const int  rearWheelCircumference_mm = wheelCircumference_mm; // identical
+const word wheelCircumference_mm     = 1540; // 406-40 wheel
+const word rearWheelCircumference_mm = wheelCircumference_mm; // identical
 #elif defined(ICB_DF)
-const int  wheelCircumference_mm = 1450;    // 406-28 wheel
-const int  rearWheelCircumference_mm = 1990; // 559-35 wheel
+const word wheelCircumference_mm     = 1450; // 406-28 wheel
+const word rearWheelCircumference_mm = 1990; // 559-35 wheel
 #endif
-const float gearOnCassette_scaling = ((float)rearWheelCircumference_mm/(float)wheelCircumference_mm);
+const float gearOnCassette_scaling  = ((float)rearWheelCircumference_mm / (float)wheelCircumference_mm);
 
-const byte speakerVolume = 50;
-const byte setBrakeMaxTimeOn_s =  15; // max time for a brakelight to be on.
-byte setOledIntensity = 0;
+const byte speakerVolume       = 50;
+const byte setBrakeMaxTimeOn_s = 15; // max time for a brakelight to be on.
+byte setOledIntensity          = 0;
 
 #if defined(ICB_DF)
-const byte setTeethOnCainring = 53; // MaartenS: 53
+const byte setTeethOnChainring = 53; // MaartenS: 53
 #elif defined(QUATRO)
-const byte setTeethOnCainring = 70; //Gert: 70, STefan: 75 MaartenS: 53
+const byte setTeethOnChainring = 70; // Gert: 70, STefan: 75 MaartenS: 53
 #else
-const byte setTeethOnCainring = 53; // MaartenS: 53
+const byte setTeethOnChainring = 53; // MaartenS: 53
 #endif
 
-
 #if defined(QUATRO)
-const byte setTeethOnCassette[20] = {11, 13, 15, 17, 19, 22, 25, 28, 32, 36,
-                                      2750,  3250,  37, 42, 47, 55, 62, 70, 80, 90
-                                     }; // Quatro Note that I put two of the gears to 100 time the number to avoid confusion with the normal low gearing (36 etc). S11 and S13 will not be displayed.
+const byte setTeethOnCassette[20] = { 11, 13, 15, 17, 19, 22, 25, 28, 32, 36,
+    2750, 3250, 37, 42, 47, 55, 62, 70, 80, 90 }; // Quatro Note that I put two of the gears to 100 time the number to avoid confusion with the normal low gearing (36 etc). S11 and S13 will not be displayed.
+/*
 const String setTeethOnCassette_string[20] = {"11", "13", "15", "17", "19", "22", "25", "28", "32", "36",
                                               "s11", "s13", "s15", "s17", "s19", "s22", "s25", "s28", "s32", "s36"
-                                             };
+                                             };*/
 #elif defined(ICB_DF)
-const byte setTeethOnCassette[10] = {11, 12, 13, 15, 17, 19, 22, 25, 28, 32};
+const byte setTeethOnCassette[10] = { 11, 12, 13, 15, 17, 19, 22, 25, 28, 32 };
+/*
 const String setTeethOnCassette_string[10] = {"11", "12", "13", "15", "17", "19", "22", "25", "28", "32"};
+*/
 #else
-const byte setTeethOnCassette[20] = {11, 13, 15, 17, 19, 22, 25, 28, 32, 36,
-                                      2750,  3250,  37, 42, 47, 55, 62, 70, 80, 90
-                                     }; // Quatro Note that I put two of the gears to 100 time the number to avoid confusion with the normal low gearing (36 etc). S11 and S13 will not be displayed.
+const byte setTeethOnCassette[20] = { 11, 13, 15, 17, 19, 22, 25, 28, 32, 36,
+    2750, 3250, 37, 42, 47, 55, 62, 70, 80, 90 }; // Quatro Note that I put two of the gears to 100 time the number to avoid confusion with the normal low gearing (36 etc). S11 and S13 will not be displayed.
+/*
 const String setTeethOnCassette_string[20] = {"11", "13", "15", "17", "19", "22", "25", "28", "32", "36",
                                               "s11", "s13", "s15", "s17", "s19", "s22", "s25", "s28", "s32", "s36"
                                              }; // Quatro
+*/
 #endif
 
 /*
-  Defaullt intensiteiten
+  Default intensiteiten
 */
-const int leftLedOffIntensity = 0; // Note that currently pwm is disabled for the blinker pins. See of this has anything to do with the failing display/.
-const int leftLedLowIntensity = 255;
-const int leftLedMediumIntensity = 255;
-const int leftLedHighIntensity = 255;
-const int leftLedMaxIntensity = 255;
+const byte leftLedOffIntensity = 0; // Note that currently pwm is disabled for the blinker pins. See of this has anything to do with the failing display/.
+const byte leftLedLowIntensity = 255;
+const byte leftLedMediumIntensity = 255;
+const byte leftLedHighIntensity = 255;
+const byte leftLedMaxIntensity = 255;
 
-const int rightLedOffIntensity = 0;
-const int rightLedLowIntensity = 255;
-const int rightLedMediumIntensity = 255;
-const int rightLedHighIntensity = 255;
-const int rightLedMaxIntensity = 255;
+const byte rightLedOffIntensity = 0;
+const byte rightLedLowIntensity = 255;
+const byte rightLedMediumIntensity = 255;
+const byte rightLedHighIntensity = 255;
+const byte rightLedMaxIntensity = 255;
 
-const int rearLedOffIntensity = 8;
-const int rearLedLowIntensity = 32;
-const int rearLedMediumIntensity = 64;
-const int rearLedHighIntensity = 64; // Note that this stops the up/down!
-const int rearLedMaxIntensity = 255;
+const byte rearLedOffIntensity = 8;
+const byte rearLedLowIntensity = 32;
+const byte rearLedMediumIntensity = 64;
+const byte rearLedHighIntensity = 64; // Note that this stops the up/down!
+const byte rearLedMaxIntensity = 255;
 
-#if  defined(STRADA)
-const int headLedOffIntensity = 0;
-const int headLedLowIntensity = 16;
-const int headLedMediumIntensity = 96;
-const int headLedHighIntensity = 255;
-const int headLedMaxIntensity = 255;
+#if defined(STRADA)
+const byte headLedOffIntensity = 0;
+const byte headLedLowIntensity = 16;
+const byte headLedMediumIntensity = 96;
+const byte headLedHighIntensity = 255;
+const byte headLedMaxIntensity = 255;
 
-const int auxLedOffIntensity = 0; //aux is just an other headlinght
-const int auxLedLowIntensity = 16;
-const int auxLedMediumIntensity = 96;
-const int auxLedHighIntensity = 255;
-const int auxLedMaxIntensity = 255;
+const byte auxLedOffIntensity = 0; //aux is just an other headlinght
+const byte auxLedLowIntensity = 16;
+const byte auxLedMediumIntensity = 96;
+const byte auxLedHighIntensity = 255;
+const byte auxLedMaxIntensity = 255;
 
 #elif defined(QUILTJE)
-const int headLedOffIntensity = 0;
-const int headLedLowIntensity = 32;
-const int headLedMediumIntensity = 128;
-const int headLedHighIntensity = 255;
-const int headLedMaxIntensity = 255;
+const byte headLedOffIntensity = 0;
+const byte headLedLowIntensity = 32;
+const byte headLedMediumIntensity = 128;
+const byte headLedHighIntensity = 255;
+const byte headLedMaxIntensity = 255;
 
-const int auxLedOffIntensity = 0; // aux is the floodlight
-const int auxLedLowIntensity = 16;
-const int auxLedMediumIntensity = 32;
-const int auxLedHighIntensity = 255;
-const int auxLedMaxIntensity = 255;
+const byte auxLedOffIntensity = 0; // aux is the floodlight
+const byte auxLedLowIntensity = 16;
+const byte auxLedMediumIntensity = 32;
+const byte auxLedHighIntensity = 255;
+const byte auxLedMaxIntensity = 255;
 
 #elif defined(QUATRO)
-const int headLedOffIntensity = 0;
-const int headLedLowIntensity = 32;
-const int headLedMediumIntensity = 96;
-const int headLedHighIntensity = 255;
-const int headLedMaxIntensity = 255;
+const byte headLedOffIntensity = 0;
+const byte headLedLowIntensity = 32;
+const byte headLedMediumIntensity = 96;
+const byte headLedHighIntensity = 255;
+const byte headLedMaxIntensity = 255;
 
-const int auxLedOffIntensity = 0; // aux is the brakelight
-const int auxLedLowIntensity = 255;
-const int auxLedMediumIntensity = 255;
-const int auxLedHighIntensity = 255;
-const int auxLedMaxIntensity = 255;
+const byte auxLedOffIntensity = 0; // aux is the brakelight
+const byte auxLedLowIntensity = 255;
+const byte auxLedMediumIntensity = 255;
+const byte auxLedHighIntensity = 255;
+const byte auxLedMaxIntensity = 255;
 
 #elif defined(ICB_DF)
-const int headLedOffIntensity = 0;
-const int headLedLowIntensity = 32;
-const int headLedMediumIntensity = 96;
-const int headLedHighIntensity = 255;
-const int headLedMaxIntensity = 255;
+const byte headLedOffIntensity = 0;
+const byte headLedLowIntensity = 32;
+const byte headLedMediumIntensity = 96;
+const byte headLedHighIntensity = 255;
+const byte headLedMaxIntensity = 255;
 
-const int auxLedOffIntensity = 0; // aux is not connected in the ICB_DF
-const int auxLedLowIntensity = 0;
-const int auxLedMediumIntensity = 0;
-const int auxLedHighIntensity = 0;
-const int auxLedMaxIntensity = 0;
+const byte auxLedOffIntensity = 0; // aux is not connected in the ICB_DF
+const byte auxLedLowIntensity = 0;
+const byte auxLedMediumIntensity = 0;
+const byte auxLedHighIntensity = 0;
+const byte auxLedMaxIntensity = 0;
 
 #endif
-
